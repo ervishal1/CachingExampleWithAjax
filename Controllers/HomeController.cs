2311c4735bbbc96a125b1c394a14e7badbb062f4
Vishal Singal <vishalsingal6@gmal.com>
2023-01-13 13:57:07 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using CachingExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Rotativa.AspNetCore;
+using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CachingExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IMemoryCache MemoryCache;
+
+        public HomeController(ILogger<HomeController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            MemoryCache = memoryCache;
+        }
+
+        public IActionResult Index(bool? val)
+        {
+            DateTime CurrentTime;
+            bool value = MemoryCache.TryGetValue("CachedTime", out CurrentTime);
+            if (!value)
+            {
+                CurrentTime = DateTime.Now;
+                var cacheEntryOptions = new MemoryCacheEntryOptions().
+                    SetSlidingExpiration(TimeSpan.FromSeconds(30));
+                MemoryCache.Set("CachedTime",CurrentTime,cacheEntryOptions);
+            }
+            if (val == true)
+            {
+                return new ViewAsPdf(CurrentTime);
+            }
+            return View(CurrentTime);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
